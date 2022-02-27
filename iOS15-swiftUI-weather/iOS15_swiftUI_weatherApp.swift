292b21f596e5af8ec7ce142c911c1af0f64b2b41
KAMONPAN KAEWHEANG <kamonpan@dit.daikin.co.jp>
2022-02-27 17:15:41 +0700

    Initial Commit

---
+++ iOS15-swiftUI-weather/iOS15_swiftUI_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS15_swiftUI_weatherApp.swift
+//  iOS15-swiftUI-weather
+//
+//  Created by admin on 27/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct iOS15_swiftUI_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
